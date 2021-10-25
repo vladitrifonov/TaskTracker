f5b528b2626dc35f1d5b83bbc150511ffd1349c0
Vladimir Trifonov <vladimir.trifonov@quipu.de>
2021-10-25 18:24:35 +0300

    try to make modal

--- TaskTracker.Application/Common/ViewModels/TaskViewModel.cs
+++ TaskTracker.Application/Common/ViewModels/TaskViewModel.cs
@@ -5,6 +5,7 @@ namespace TaskTracker.Application.Common.ViewModels
     public class TaskViewModel
     {
         public int Id { get; set; }
+        public int ProjectId { get; set; }
         public string Name { get; set; }
         public TaskStatus Status { get; set; }
         public string Description { get; set; }
